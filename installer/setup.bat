1c0e113fba0e2de1074c7e2c167d391317b3e6c6
Jerry Yang <yxjsolid@gmail.com>
2014-11-06 14:09:39 +0800

    update

--- installer/setup.bat
+++ installer/setup.bat
@@ -1,6 +1,6 @@
 @echo off
 cd PyInstaller-2.1
-pyinstaller.py -F ..\..\imageCodePractice.py --workpath="..\..\tmp" --distpath="..\..\tmp\dist"
+pyinstaller.py -F ..\..\imageCodePractice.py --workpath="..\..\tmp" --distpath="..\..\dist" --specpath="..\..\tmp"
 
 
 echo input any key to quit
