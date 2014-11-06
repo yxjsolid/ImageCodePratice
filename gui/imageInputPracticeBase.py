78474c09d128acdb0f96e7682cd8d1d403ff91b0
Jerry Yang <yxjsolid@gmail.com>
2014-11-06 18:17:21 +0800

    update

--- gui/imageInputPracticeBase.py
+++ gui/imageInputPracticeBase.py
@@ -21,18 +21,73 @@ class imgInputPracticeBase ( wx.Frame ):
 		
 		self.SetSizeHintsSz( wx.DefaultSize, wx.DefaultSize )
 		
+		gbSizer2 = wx.GridBagSizer( 0, 0 )
+		gbSizer2.SetFlexibleDirection( wx.BOTH )
+		gbSizer2.SetNonFlexibleGrowMode( wx.FLEX_GROWMODE_SPECIFIED )
+		
+		self.m_panel2 = wx.Panel( self, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.TAB_TRAVERSAL )
+		gbSizer3 = wx.GridBagSizer( 0, 0 )
+		gbSizer3.SetFlexibleDirection( wx.BOTH )
+		gbSizer3.SetNonFlexibleGrowMode( wx.FLEX_GROWMODE_SPECIFIED )
+		
+		sbSizer4 = wx.StaticBoxSizer( wx.StaticBox( self.m_panel2, wx.ID_ANY, u"模式" ), wx.VERTICAL )
+		
+		self.m_radioBtn1 = wx.RadioButton( self.m_panel2, wx.ID_ANY, u"练习模式", wx.DefaultPosition, wx.DefaultSize, 0 )
+		self.m_radioBtn1.SetValue( True ) 
+		sbSizer4.Add( self.m_radioBtn1, 0, wx.ALL, 5 )
+		
+		self.m_radioBtn2 = wx.RadioButton( self.m_panel2, wx.ID_ANY, u"2模式", wx.DefaultPosition, wx.DefaultSize, 0 )
+		sbSizer4.Add( self.m_radioBtn2, 0, wx.ALL, 5 )
+		
+		
+		gbSizer3.Add( sbSizer4, wx.GBPosition( 0, 0 ), wx.GBSpan( 1, 1 ), wx.EXPAND, 5 )
+		
+		sbSizer1 = wx.StaticBoxSizer( wx.StaticBox( self.m_panel2, wx.ID_ANY, u"统计" ), wx.VERTICAL )
+		
+		self.m_panel4 = wx.Panel( self.m_panel2, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.TAB_TRAVERSAL )
+		self.statSizer = wx.BoxSizer( wx.VERTICAL )
+		
+		
+		self.m_panel4.SetSizer( self.statSizer )
+		self.m_panel4.Layout()
+		self.statSizer.Fit( self.m_panel4 )
+		sbSizer1.Add( self.m_panel4, 1, wx.EXPAND |wx.ALL, 5 )
+		
+		
+		gbSizer3.Add( sbSizer1, wx.GBPosition( 1, 0 ), wx.GBSpan( 1, 1 ), wx.EXPAND, 5 )
+		
+		self.m_panel5 = wx.Panel( self.m_panel2, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.TAB_TRAVERSAL )
 		gbSizer16 = wx.GridBagSizer( 0, 0 )
 		gbSizer16.SetFlexibleDirection( wx.BOTH )
 		gbSizer16.SetNonFlexibleGrowMode( wx.FLEX_GROWMODE_SPECIFIED )
 		
-		self.imgBmp = wx.StaticBitmap( self, wx.ID_ANY, wx.NullBitmap, wx.DefaultPosition, wx.DefaultSize, 0 )
+		self.imgBmp = wx.StaticBitmap( self.m_panel5, wx.ID_ANY, wx.NullBitmap, wx.DefaultPosition, wx.DefaultSize, 0 )
 		gbSizer16.Add( self.imgBmp, wx.GBPosition( 0, 0 ), wx.GBSpan( 1, 1 ), wx.ALL, 5 )
 		
-		self.answerTxt = wx.TextCtrl( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0 )
+		self.answerTxt = wx.TextCtrl( self.m_panel5, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, wx.TE_PROCESS_ENTER )
 		gbSizer16.Add( self.answerTxt, wx.GBPosition( 1, 0 ), wx.GBSpan( 1, 1 ), wx.ALL, 5 )
 		
+		self.clockText = wx.StaticText( self.m_panel5, wx.ID_ANY, u"MyLabel", wx.DefaultPosition, wx.DefaultSize, 0 )
+		self.clockText.Wrap( -1 )
+		gbSizer16.Add( self.clockText, wx.GBPosition( 1, 1 ), wx.GBSpan( 1, 1 ), wx.ALL, 5 )
 		
-		self.SetSizer( gbSizer16 )
+		
+		self.m_panel5.SetSizer( gbSizer16 )
+		self.m_panel5.Layout()
+		gbSizer16.Fit( self.m_panel5 )
+		gbSizer3.Add( self.m_panel5, wx.GBPosition( 0, 1 ), wx.GBSpan( 1, 1 ), wx.EXPAND |wx.ALL, 5 )
+		
+		
+		self.m_panel2.SetSizer( gbSizer3 )
+		self.m_panel2.Layout()
+		gbSizer3.Fit( self.m_panel2 )
+		gbSizer2.Add( self.m_panel2, wx.GBPosition( 0, 0 ), wx.GBSpan( 1, 1 ), wx.EXPAND |wx.ALL, 5 )
+		
+		self.m_panel3 = wx.Panel( self, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.TAB_TRAVERSAL )
+		gbSizer2.Add( self.m_panel3, wx.GBPosition( 0, 1 ), wx.GBSpan( 1, 1 ), wx.EXPAND |wx.ALL, 5 )
+		
+		
+		self.SetSizer( gbSizer2 )
 		self.Layout()
 		
 		self.Centre( wx.BOTH )
@@ -47,5 +102,167 @@ class imgInputPracticeBase ( wx.Frame ):
 	# Virtual event handlers, overide them in your derived class
 	def onAnswer( self, event ):
 		event.Skip()
+	
+
+###########################################################################
+## Class MyFrame2
+###########################################################################
+
+class MyFrame2 ( wx.Frame ):
+	
+	def __init__( self, parent ):
+		wx.Frame.__init__ ( self, parent, id = wx.ID_ANY, title = wx.EmptyString, pos = wx.DefaultPosition, size = wx.Size( 500,300 ), style = wx.DEFAULT_FRAME_STYLE|wx.TAB_TRAVERSAL )
+		
+		self.SetSizeHintsSz( wx.DefaultSize, wx.DefaultSize )
+		
+		gbSizer2 = wx.GridBagSizer( 0, 0 )
+		gbSizer2.SetFlexibleDirection( wx.BOTH )
+		gbSizer2.SetNonFlexibleGrowMode( wx.FLEX_GROWMODE_SPECIFIED )
+		
+		self.m_panel2 = wx.Panel( self, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.TAB_TRAVERSAL )
+		gbSizer3 = wx.GridBagSizer( 0, 0 )
+		gbSizer3.SetFlexibleDirection( wx.BOTH )
+		gbSizer3.SetNonFlexibleGrowMode( wx.FLEX_GROWMODE_SPECIFIED )
+		
+		sbSizer1 = wx.StaticBoxSizer( wx.StaticBox( self.m_panel2, wx.ID_ANY, u"label" ), wx.VERTICAL )
+		
+		self.m_panel4 = wx.Panel( self.m_panel2, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.TAB_TRAVERSAL )
+		bSizer1 = wx.BoxSizer( wx.VERTICAL )
+		
+		bSizer2 = wx.BoxSizer( wx.HORIZONTAL )
+		
+		self.m_staticText16 = wx.StaticText( self.m_panel4, wx.ID_ANY, u"总数：", wx.DefaultPosition, wx.DefaultSize, 0 )
+		self.m_staticText16.Wrap( -1 )
+		bSizer2.Add( self.m_staticText16, 0, wx.ALL, 5 )
+		
+		self.m_staticText17 = wx.StaticText( self.m_panel4, wx.ID_ANY, u"0", wx.DefaultPosition, wx.DefaultSize, 0 )
+		self.m_staticText17.Wrap( -1 )
+		bSizer2.Add( self.m_staticText17, 0, wx.ALL, 5 )
+		
+		
+		bSizer1.Add( bSizer2, 1, wx.EXPAND, 5 )
+		
+		bSizer21 = wx.BoxSizer( wx.HORIZONTAL )
+		
+		self.m_staticText161 = wx.StaticText( self.m_panel4, wx.ID_ANY, u"正确：", wx.DefaultPosition, wx.DefaultSize, 0 )
+		self.m_staticText161.Wrap( -1 )
+		bSizer21.Add( self.m_staticText161, 0, wx.ALL, 5 )
+		
+		self.m_staticText171 = wx.StaticText( self.m_panel4, wx.ID_ANY, u"100", wx.DefaultPosition, wx.DefaultSize, 0 )
+		self.m_staticText171.Wrap( -1 )
+		bSizer21.Add( self.m_staticText171, 0, wx.ALL, 5 )
+		
+		
+		bSizer1.Add( bSizer21, 1, wx.EXPAND, 5 )
+		
+		bSizer211 = wx.BoxSizer( wx.HORIZONTAL )
+		
+		self.m_staticText1611 = wx.StaticText( self.m_panel4, wx.ID_ANY, u"错误", wx.DefaultPosition, wx.DefaultSize, 0 )
+		self.m_staticText1611.Wrap( -1 )
+		bSizer211.Add( self.m_staticText1611, 0, wx.ALL, 5 )
+		
+		self.m_staticText1711 = wx.StaticText( self.m_panel4, wx.ID_ANY, u"MyLabel", wx.DefaultPosition, wx.DefaultSize, 0 )
+		self.m_staticText1711.Wrap( -1 )
+		bSizer211.Add( self.m_staticText1711, 0, wx.ALL, 5 )
+		
+		
+		bSizer1.Add( bSizer211, 1, wx.EXPAND, 5 )
+		
+		bSizer2111 = wx.BoxSizer( wx.HORIZONTAL )
+		
+		self.m_staticText16111 = wx.StaticText( self.m_panel4, wx.ID_ANY, u"MyLabel", wx.DefaultPosition, wx.DefaultSize, 0 )
+		self.m_staticText16111.Wrap( -1 )
+		bSizer2111.Add( self.m_staticText16111, 0, wx.ALL, 5 )
+		
+		self.m_staticText17111 = wx.StaticText( self.m_panel4, wx.ID_ANY, u"MyLabel", wx.DefaultPosition, wx.DefaultSize, 0 )
+		self.m_staticText17111.Wrap( -1 )
+		bSizer2111.Add( self.m_staticText17111, 0, wx.ALL, 5 )
+		
+		
+		bSizer1.Add( bSizer2111, 1, wx.EXPAND, 5 )
+		
+		bSizer21111 = wx.BoxSizer( wx.HORIZONTAL )
+		
+		self.m_staticText161111 = wx.StaticText( self.m_panel4, wx.ID_ANY, u"MyLabel", wx.DefaultPosition, wx.DefaultSize, 0 )
+		self.m_staticText161111.Wrap( -1 )
+		bSizer21111.Add( self.m_staticText161111, 0, wx.ALL, 5 )
+		
+		self.m_staticText171111 = wx.StaticText( self.m_panel4, wx.ID_ANY, u"MyLabel", wx.DefaultPosition, wx.DefaultSize, 0 )
+		self.m_staticText171111.Wrap( -1 )
+		bSizer21111.Add( self.m_staticText171111, 0, wx.ALL, 5 )
+		
+		
+		bSizer1.Add( bSizer21111, 1, wx.EXPAND, 5 )
+		
+		bSizer21112 = wx.BoxSizer( wx.HORIZONTAL )
+		
+		self.m_staticText161112 = wx.StaticText( self.m_panel4, wx.ID_ANY, u"MyLabel", wx.DefaultPosition, wx.DefaultSize, 0 )
+		self.m_staticText161112.Wrap( -1 )
+		bSizer21112.Add( self.m_staticText161112, 0, wx.ALL, 5 )
+		
+		self.m_staticText171112 = wx.StaticText( self.m_panel4, wx.ID_ANY, u"MyLabel", wx.DefaultPosition, wx.DefaultSize, 0 )
+		self.m_staticText171112.Wrap( -1 )
+		bSizer21112.Add( self.m_staticText171112, 0, wx.ALL, 5 )
+		
+		
+		bSizer1.Add( bSizer21112, 1, wx.EXPAND, 5 )
+		
+		bSizer21113 = wx.BoxSizer( wx.HORIZONTAL )
+		
+		self.m_staticText161113 = wx.StaticText( self.m_panel4, wx.ID_ANY, u"MyLabel", wx.DefaultPosition, wx.DefaultSize, 0 )
+		self.m_staticText161113.Wrap( -1 )
+		bSizer21113.Add( self.m_staticText161113, 0, wx.ALL, 5 )
+		
+		self.m_staticText171113 = wx.StaticText( self.m_panel4, wx.ID_ANY, u"MyLabel", wx.DefaultPosition, wx.DefaultSize, 0 )
+		self.m_staticText171113.Wrap( -1 )
+		bSizer21113.Add( self.m_staticText171113, 0, wx.ALL, 5 )
+		
+		
+		bSizer1.Add( bSizer21113, 1, wx.EXPAND, 5 )
+		
+		
+		self.m_panel4.SetSizer( bSizer1 )
+		self.m_panel4.Layout()
+		bSizer1.Fit( self.m_panel4 )
+		sbSizer1.Add( self.m_panel4, 1, wx.EXPAND |wx.ALL, 5 )
+		
+		
+		gbSizer3.Add( sbSizer1, wx.GBPosition( 0, 0 ), wx.GBSpan( 1, 1 ), wx.EXPAND, 5 )
+		
+		self.m_panel5 = wx.Panel( self.m_panel2, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.TAB_TRAVERSAL )
+		gbSizer16 = wx.GridBagSizer( 0, 0 )
+		gbSizer16.SetFlexibleDirection( wx.BOTH )
+		gbSizer16.SetNonFlexibleGrowMode( wx.FLEX_GROWMODE_SPECIFIED )
+		
+		self.imgBmp = wx.StaticBitmap( self.m_panel5, wx.ID_ANY, wx.NullBitmap, wx.DefaultPosition, wx.DefaultSize, 0 )
+		gbSizer16.Add( self.imgBmp, wx.GBPosition( 0, 0 ), wx.GBSpan( 1, 1 ), wx.ALL, 5 )
+		
+		self.answerTxt = wx.TextCtrl( self.m_panel5, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0 )
+		gbSizer16.Add( self.answerTxt, wx.GBPosition( 1, 0 ), wx.GBSpan( 1, 1 ), wx.ALL, 5 )
+		
+		self.clockText = wx.StaticText( self.m_panel5, wx.ID_ANY, u"MyLabel", wx.DefaultPosition, wx.DefaultSize, 0 )
+		self.clockText.Wrap( -1 )
+		gbSizer16.Add( self.clockText, wx.GBPosition( 1, 1 ), wx.GBSpan( 1, 1 ), wx.ALL, 5 )
+		
+		
+		self.m_panel5.SetSizer( gbSizer16 )
+		self.m_panel5.Layout()
+		gbSizer16.Fit( self.m_panel5 )
+		gbSizer3.Add( self.m_panel5, wx.GBPosition( 0, 1 ), wx.GBSpan( 1, 1 ), wx.EXPAND |wx.ALL, 5 )
+		
+		
+		self.m_panel2.SetSizer( gbSizer3 )
+		self.m_panel2.Layout()
+		gbSizer3.Fit( self.m_panel2 )
+		gbSizer2.Add( self.m_panel2, wx.GBPosition( 0, 0 ), wx.GBSpan( 1, 1 ), wx.EXPAND |wx.ALL, 5 )
+		
+		
+		self.SetSizer( gbSizer2 )
+		self.Layout()
+		
+		self.Centre( wx.BOTH )
+	
+	def __del__( self ):
+		pass
 	
 
--- imageCodePractice.py
+++ imageCodePractice.py
@@ -2,24 +2,67 @@
 from gui.imageInputPracticeBase import *
 import os
 import sys
+import time
+import threading
+import random
 
 class imgInputPractice(imgInputPracticeBase):
+
+    STAT_ALL_CNT, STAT_CORRECT_CNT, STAT_ERR_CNT, STAT_ACCURATE, STAT_FASTEST, STAT_SLOWEST = range(6)
+
     def __init__(self, parent):
         imgInputPracticeBase.__init__(self, parent)
-        self.index = 0
+
+
+        self.statsInfo = [
+                            (self.STAT_ALL_CNT,      u"总数:"),
+                            (self.STAT_ERR_CNT,      u"错误:"),
+                            (self.STAT_ACCURATE,       u"准确率:"),
+                            (self.STAT_FASTEST,      u"最快:"),
+                            (self.STAT_SLOWEST,      u"最慢:"),
+                          ]
+
+        self.statsDic = {}
         self.fileList = []
+        self.fileCnt = 0
         self.currentCode = None
+        self.currentIndex = 0
+        self.clockBeginTime = 0
+
+        self.totalCnt = 0
+        self.errCnt = 0
+        self.correctCnt = 0
+        self.fast = 9999
+        self.slow = 0
+        self.accurate = 0
+        self.inError = 0
+
+
+        self.addAllStats()
+
         self.getAllFileList()
         self.displayImage()
 
-    def displayImage(self):
-        imgPath = self.fileList[self.index]
-        self.index += 1
+        self.doClock()
 
+
+    def getNextImageIndex(self):
+        index = random.randint(0, self.fileCnt - 1)
+        return index
+
+
+    def resetClock(self):
+        self.clockBeginTime = time.time()
+
+
+    def displayImage(self):
+        self.currentIndex = self.getNextImageIndex()
+        imgPath = self.fileList[self.currentIndex]
         img = wx.Image(imgPath)
         #self.imgBmp.SetBitmapLabel(wx.BitmapFromImage(img))
         self.imgBmp.SetBitmap(wx.BitmapFromImage(img))
         self.imgBmp.Fit()
+        self.resetClock()
         self.currentCode = os.path.split(imgPath)[1].split(".")[0]
         pass
 
@@ -41,28 +84,98 @@ class imgInputPractice(imgInputPracticeBase):
             if os.path.isfile(path):
                 self.fileList.append(path)
 
+        self.fileCnt = len(self.fileList)
+
 
     def onAnswer( self, event ):
+        print "onAnsser"
         answer = self.answerTxt.GetValue()
-
-
         if answer == self.currentCode:
+            self.correctCnt += 1
+            diff = time.time() - self.clockBeginTime
+            self.fast = min(diff, self.fast)
+            self.slow = max(diff, self.slow)
+
+            print "spend %f s"%(diff)
             self.displayImage()
             self.answerTxt.Clear()
+            self.inError = 0
+            self.totalCnt += 1
         else:
+            if not self.inError:
+                self.errCnt += 1
+                self.inError = 1
+
             wx.MessageBox(u'错了','Error',wx.OK|wx.ICON_ERROR)
             print "\n error"
             print "input:", answer
             print "correct:", self.currentCode
 
+        self.updateAllStats()
 
+    def updateStats(self, statType, stat):
+        text = self.statsDic[statType]
+        text.SetLabel(stat)
 
+    def updateAllStats(self):
+        self.updateStats(self.STAT_ALL_CNT,str(self.totalCnt))
+        self.updateStats(self.STAT_ERR_CNT, str(self.errCnt))
+        if self.totalCnt:
+            rate = "%.1f%%"%((self.totalCnt - self.errCnt)/float(self.totalCnt)*100)
+        else:
+            rate = "0"
 
-
-
+        self.updateStats(self.STAT_ACCURATE, rate)
+        slow = "%.2f s"%(self.slow)
+        self.updateStats(self.STAT_SLOWEST, slow)
+        fast = "%.2f s"%(self.fast)
+        self.updateStats(self.STAT_FASTEST, fast)
 
         pass
 
+    def __doClock(self):
+        diff = 0
+        while True:
+            label = "%f"%diff
+            self.clockText.SetLabel(label)
+            time.sleep(0.01)
+            diff = time.time() - self.clockBeginTime
+        pass
+
+
+    def doClock(self):
+        self.__runTask(self.__doClock, ())
+
+
+    def __runTask(self, func, arg):
+        tsk = threading.Thread(target=func, args=arg)
+        tsk.start()
+        return tsk
+
+
+    def addStatElem(self, statInfo):
+        statId, label = statInfo
+        bSizer = wx.BoxSizer( wx.HORIZONTAL )
+        statNameText = wx.StaticText( self.m_panel4, wx.ID_ANY, label, wx.DefaultPosition, wx.DefaultSize, 0 )
+        statNameText.Wrap( -1 )
+        statNameText.SetMinSize( wx.Size( 40,-1 ) )
+        bSizer.Add( statNameText, 0, wx.ALL, 5 )
+
+        #index = str(random.randint(0, 1000))
+
+        statDispText = wx.StaticText( self.m_panel4, wx.ID_ANY, "0", wx.DefaultPosition, wx.DefaultSize, 0 )
+        statDispText.Wrap( -1 )
+        statDispText.SetMinSize( wx.Size( 40,-1 ) )
+
+        bSizer.Add( statDispText, 0, wx.ALL, 5 )
+        self.statSizer.Add( bSizer, 1, wx.EXPAND, 5 )
+
+        self.statsDic[statId] = statDispText
+
+
+    def addAllStats(self):
+        for statInfo in self.statsInfo:
+            self.addStatElem(statInfo)
 
 
 if __name__ == "__main__":
